7623f7b35171b265cf712c10f9eb56917a08a32d
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2024-06-23 09:19:57 +0530

    Add files via upload

---
+++ Infra_automation_with_terraform_functions/var.tf
@@ -0,0 +1,36 @@
+# Define your region
+variable "region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-2"
+}
+
+# Data source to get available AWS availability zones
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+#-------------------------Fetch AMI ID---------------------
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+# Data source to get the current AWS account ID
+data "aws_caller_identity" "current" {}
