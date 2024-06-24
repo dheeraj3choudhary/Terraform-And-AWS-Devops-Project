c230da7fdcfa9e95218c7af8d673eee43722c600
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2024-06-24 06:43:00 +0530

    Add files via upload

---
+++ Infra_automation_with_terraform_functions/main.tf
@@ -0,0 +1,84 @@
+# Create a VPC
+resource "aws_vpc" "customvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "CustomVPC"
+  }
+}
+
+# Create Public Subnets
+resource "aws_subnet" "customsub" {
+  count  = length(data.aws_availability_zones.available.names)
+  vpc_id = aws_vpc.customvpc.id
+
+  cidr_block        = "10.0.${count.index}.0/24"
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-${element(data.aws_availability_zones.available.names, count.index)}"
+    Type = "Public"
+  }
+}
+
+# Create EC2 instances in the public subnets
+resource "aws_instance" "custom_instances" {
+  count         = length(aws_subnet.customsub)
+  ami           = data.aws_ami.latest_amazon_linux.id
+  instance_type = "t2.micro"
+  subnet_id     = element(aws_subnet.customsub[*].id, count.index)
+
+  tags = {
+    Name = "ServerNo-${count.index}"
+    Env  = "Dev"
+  }
+}
+
+# resource "aws_vpc" "customvpc" {
+#   cidr_block       = "10.0.0.0/16"
+#   instance_tenancy = "default"
+
+#   tags = {
+#     Name = "main"
+#   }
+# }
+
+# resource "aws_subnet" "customsub" {
+#   count  = length(data.aws_availability_zones.available.names)
+#   vpc_id = aws_vpc.customvpc.id
+
+#   cidr_block        = "10.0.${count.index}.0/24"
+#   availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+#   tags = {
+#     Name = "public-${element(data.aws_availability_zones.available.names, count.index)}"
+#     Type = "Public"
+#   }
+# }
+
+# data "aws_subnets" "GetSubnet" {
+#   depends_on = [aws_subnet.customsub]
+#   filter {
+#     name   = "vpc-id"
+#     values = [aws_vpc.customvpc.id]
+#   }
+#   filter {
+#     name   = "tag:Type"
+#     values = ["Public"]
+#   }
+# }
+
+# resource "aws_instance" "custom_instances" {
+#   depends_on    = [aws_subnet.customsub, data.aws_subnets.GetSubnet]
+#   ami           = data.aws_ami.latest_amazon_linux.id
+#   for_each      = toset(data.aws_subnets.GetSubnet.ids)
+#   subnet_id     = each.value
+#   instance_type = "t2.micro"
+#   tags = {
+#     Name = "ServerNo-"
+#     Env  = "Dev"
+#   }
+# }
