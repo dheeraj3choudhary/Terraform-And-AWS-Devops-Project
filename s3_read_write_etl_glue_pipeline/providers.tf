fb6623f68506ddbfe00b64a6a0454a344036a42f
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2024-07-08 09:19:15 +0530

    Add files via upload

--- s3_read_write_etl_glue_pipeline/providers.tf
+++ s3_read_write_etl_glue_pipeline/providers.tf
@@ -1,4 +1,4 @@
 provider "aws" {
-  region                   = "us-west-2"
-  shared_credentials_files = ["<Path to your AWS Credentials File>"]
+  region                   = var.aws_region
+  shared_credentials_files = ["C:/Users/DHEERA~1/.aws/credentials"]
 }
